40d15b7fd3d0e36a0535180661a731d123d1eba1
ClaudomiroSales <claudomiro.sales@gmail.com>
2022-02-21 11:25:47 -0300

    Update 02-Variables.js
    Tornando mais fácil de mostrar o arquivo em sala de aula. Ordem e organização.

--- LearningJavaScriptDataStructuresandAlgorithmsThirdEdition_Code/examples/chapter01_02/02-Variables.js
+++ LearningJavaScriptDataStructuresandAlgorithmsThirdEdition_Code/examples/chapter01_02/02-Variables.js
@@ -3,7 +3,6 @@
 
 var num = 1; // {1}
 num = 3; // {2}
-
 var price = 1.5; // {3}
 var myName = 'Packt'; // {4}
 var trueValue = true; // {5}
@@ -11,9 +10,9 @@ var nullVar = null; // {6}
 var und; // {7}
 
 console.log('num: ' + num);
+console.log('price: ' + price);
 console.log('myName: ' + myName);
 console.log('trueValue: ' + trueValue);
-console.log('price: ' + price);
 console.log('nullVar: ' + nullVar);
 console.log('und: ' + und);
 
@@ -34,7 +33,6 @@ function myOtherFunction() {
 
 console.log(myVariable); //{1}
 console.log(myFunction()); //{2}
-
 console.log(myOtherVariable); //{3}
 console.log(myOtherFunction()); //{4}
 console.log(myOtherVariable); //{5}
