c07bb359b06db41a133922227fb275a60e67ad62
ClaudomiroSales <claudomiro.sales@gmail.com>
2022-02-21 11:52:23 -0300

    Update 03-Operators.js
    Tornando mais fácil de mostrar o arquivo em sala de aula. Ordem e organização.

--- LearningJavaScriptDataStructuresandAlgorithmsThirdEdition_Code/examples/chapter01_02/03-Operators.js
+++ LearningJavaScriptDataStructuresandAlgorithmsThirdEdition_Code/examples/chapter01_02/03-Operators.js
@@ -2,35 +2,37 @@
 /* eslint-disable */
 
 /* Arithmetic operators */
+console.log('- Arithmetic operators');
 var num = 0; // {1}
-console.log('num value is ' + num);
+console.log('var num = 0; => ' + num);
 
 num = num + 2;
-console.log('New num value is ' + num);
+console.log('num = num + 2; => ' + num);
 
 num = num * 3;
-console.log('New num value is ' + num);
+console.log('num = num * 3; => ' + num);
 
 num = num / 2;
-console.log('New num value is ' + num);
+console.log('num = num / 2; => ' + num);
 
 num++;
 num--;
+console.log('num++; num--; => ' + num);
 
-console.log('New num value is ' + num);
-
-console.log('num mod 2 value is ' + num % 2);
+console.log('num % 2; => ' + num % 2);
 
 /* Assignment operators */
+console.log('- Assignment operators');
+console.log('num += 1; num -= 2; num *= 3; num /= 2; num %= 3;');
 num += 1;
 num -= 2;
 num *= 3;
 num /= 2;
 num %= 3;
-
 console.log('New num value is ' + num);
 
-/* Assignment operators */
+/*Comparison operators */
+console.log('- Comparison operators');
 console.log('num == 1 : ' + (num == 1));
 console.log('num === 1 : ' + (num === 1));
 console.log('num != 1 : ' + (num != 1));
@@ -40,11 +42,13 @@ console.log('num >= 1 : ' + (num >= 1));
 console.log('num <= 1 : ' + (num <= 1));
 
 /* Logical operators */
+console.log('- Logical operators');
 console.log('true && false : ' + (true && false));
 console.log('true || false : ' + (true || false));
 console.log('!true : ' + !true);
 
 /* Bitwise operators */
+console.log('- Bitwise operators');
 console.log('5 & 1:', 5 & 1); // same as 0101 & 0001 (result 0001 / 1)
 console.log('5 | 1:', 5 | 1); // same as 0101 | 0001 (result 0101 / 5)
 console.log('~ 5:', ~5); // same as ~0101 (result 1010 / 10)
@@ -53,6 +57,7 @@ console.log('5 << 1:', 5 << 1); // same as 0101 << 1 (result 1010 / 10)
 console.log('5 >> 1:', 5 >> 1); // same as 0101 >> 1 (result 0010 / 2)
 
 /* typeOf */
+console.log('- typeof');
 console.log('typeof num:', typeof num);
 console.log('typeof Packt:', typeof 'Packt');
 console.log('typeof true:', typeof true);
@@ -60,6 +65,8 @@ console.log('typeof [1,2,3]:', typeof [1, 2, 3]);
 console.log('typeof {name:John}:', typeof { name: 'John' });
 
 /* delete */
+console.log('- delete');
+console.log('var myObj = { name: John, age: 21 }; delete myObj.age');
 var myObj = { name: 'John', age: 21 };
 delete myObj.age;
 console.log(myObj); // Object {name: "John"}
